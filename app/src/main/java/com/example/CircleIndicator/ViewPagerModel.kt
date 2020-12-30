4072eff67fcca91ec66ecbcff47bc9d737783f84
FzBlackduck <49480021+FzBlackduck@users.noreply.github.com>
2020-12-30 11:29:30 +0700

    DetailProduct - edit

---
+++ app/src/main/java/com/example/CircleIndicator/ViewPagerModel.kt
@@ -0,0 +1,12 @@
+package com.example.CircleIndicator
+
+data class Product(
+    val name: String,
+    val price: String,
+    val quantity: String,
+    val status: String,
+    val image: String,
+    val category:String
+){
+
+}
